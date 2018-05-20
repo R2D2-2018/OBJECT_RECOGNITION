5a8424d688faf5541ffcad4a058fb164c62c0d70
arsalan <arsalan.anwari@student.hu.nl>
2018-05-20 14:26:33 +0200

    Title: Image viewer and parser feature Short: I made a new feature of vertical slice 2. Detail: This feature contains the basic code layout of the window class, with the custom added feature of a image viewer and image parser. The window class is a widow manager that can be used to display Graphical or Text user interfaces. This window manager uses OpenCV's own HI-Gui module for the Graphical interface and <iostream> for the text interface.
    Notice message: Currently no library or module is used to drive the text user interface. In the future i wanted to used a library that
    can display colored text on the console with custom headers etc.
    
    Changes: No changes were made
    Fixes: Fixed the user input poll glitch of sample_image.cpp. Also ensured the user input will be parsed as a cv::String instead of a std::String for
    compatibility with openCV.
    
    Author: Arsalan Anwari
    Team: Scrum team 6
    Date: 20-05-18

---
+++ src/window/image_viewer.hpp
@@ -0,0 +1,44 @@
+/**
+ * @brief This file contains an implementation of the global Gui interface
+ * @details The class made can be used to display an image on the display
+ * @file image_viewer.hpp
+ * @author Arsalan Anwari
+ * @date 2018-05-19
+ */
+
+#ifndef IMAGE_VIEWER_HPP
+#define IMAGE_VIEWER_HPP
+
+#include "interface/gui.hpp"
+#include "../image/image.hpp"
+
+namespace lucidy{
+
+/**
+ * @brief ImageViewer implementation
+ * @details The window manager is made in the global Gui interface and the settings are changed throughout this class
+ */
+class ImageViewer: public Gui{
+
+public:
+    ImageViewer();
+    ImageViewer(settings::WIN::GUI::data & settings);
+
+    /**
+     * @brief This function is used display a image on a display
+     * @param image 
+     */
+    void show(Image & image);
+
+    /**
+     * @brief This function is used to change the settings of the ImageViewer
+     * @details in reality the settings are being passed trough and changed in the global gui interface
+     * @param settings : settings::WIN:GUI:data&
+     */
+    void changeSettings(settings::WIN::GUI::data & settings);
+
+};
+
+}
+
+#endif //IMAGE_VIEWER_HPP
