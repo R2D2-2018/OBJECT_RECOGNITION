98e04060c40300c3f118ea3f9692bbac0d68230f
dylrak <dylrak@hotmail.com>
2018-06-01 12:14:52 +0200

    Refactored CMakeLists and Sources to better include catch.hpp, deleted catch.hpp from test folder and added .clang-format files to .gitignore

--- Sources.cmake
+++ Sources.cmake
@@ -2,6 +2,16 @@
 
 link_libraries (gcc)
 
+add_library(OpenCV SHARED IMPORTED)
+set_property(TARGET OpenCV PROPERTY IMPORTED_LOCATION "/usr/local/lib")
+find_package( OpenCV REQUIRED )
+
+set (opencv ${build_environment}/libraries/opencv)
+include_directories (${opencv}/include/opencv2/)
+
+set (catch ${build_environment}/libraries/Catch2)
+include_directories (${catch}/single_include)
+
 # Source Files:
 
 set (sources
