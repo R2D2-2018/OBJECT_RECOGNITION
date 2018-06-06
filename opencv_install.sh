556283f30c4d8bc6e5b97ac25a6a514fbc25b9a3
dylrak <dylrak@hotmail.com>
2018-06-06 15:16:58 +0200

    Included dependencies in OpenCV installation script

--- opencv_install.sh
+++ opencv_install.sh
@@ -1,3 +1,7 @@
+#Install dependencies
+sudo apt-get install build-essential qt5-default
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 # Go to libraries folder
 cd ../../libraries
 # Extract OpenCV and OpenCV_contrib
