559be498b15d27aae9c1abf06c1c79b2b1579f46
RoxanneMango <roxannevdpol@gmail.com>
2018-06-22 13:28:49 +0200

    > Removed everything except for the bare minimum to build openCV

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,8 @@
-#include "image/sample_image.hpp"
-#include "window/image_viewer.hpp"
 #include <iostream>
 
-int main(int argc, char **argv) {
-    lucidy::SampleImage img;
-    if (argc != 2 || !img.set(argv[1])) {
-        std::cout << "File not found or specified." << std::endl;
-        img.set();
-    }
-    lucidy::ImageViewer window;
-    window.show(img);
-    char k = cv::waitKey(0);
-    if (k == 27) /// escape
-    {
-        cv::destroyAllWindows();
-    }
-    return 0;
+int main() {
+	std::cout << "Hello world!\n";
+	
+	return 0;
+	
 }
