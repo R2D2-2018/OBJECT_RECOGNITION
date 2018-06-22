6e51205d8cb7231e2e4a81fdde66cb88e87817f2
RoxanneMango <roxannevdpol@gmail.com>
2018-06-22 17:55:29 +0200

    > Added Doxygen documentation to sample_image.hpp and sample_image.cpp

--- src/sample_image.cpp
+++ src/sample_image.cpp
@@ -7,5 +7,44 @@
 
 #include "sample_image.hpp"
 
-SampleImage::SampleImage() {
+///< Constructors
+SampleImage::SampleImage() : isValid(false) {
+}
+SampleImage::SampleImage(const char *path) : path(path) {
+    image = cv::imread(path, -1);
+    if (!image.data) {
+        std::cout << "Invalid image path!\n";
+        isValid = false;
+    } else {
+        isValid = true;
+    }
+}
+
+///< Setters
+void SampleImage::set(const char *newPath) {
+    path = newPath;
+    image = cv::imread(path, -1);
+    if (!image.data) {
+        std::cout << "Invalid image path!\n";
+        isValid = false;
+    }
+}
+void SampleImage::set(SampleImage &other) {
+    if (other.isValid()) {
+        path = other.getPath();
+        image = cv::imread(path, -1);
+    } else {
+        std::cout << "Other SampleImage image path was invalid!\n";
+    }
+}
+
+///< Getters
+char *SampleImage::getPath() {
+    return path;
+}
+cv::mat getImage() {
+    return image;
+}
+bool SampleImage::isValid() {
+    return isValid;
 }
--- src/sample_image.hpp
+++ src/sample_image.hpp
@@ -10,8 +10,73 @@
 
 class SampleImage {
   private:
+    const char *path; ///< Path of image.
+    bool isValid;     ///< Whether an image path is valid
+    cv::Mat image;    ///< Image object which has a path
+
   public:
+    /**
+     * @brief Constructor of SampleImage class
+     *
+     * Creates a SampleImage object with no initialised parameters such as path or image. Sets isValid automatically to false.
+     */
     SampleImage();
+    /**
+     * @brief Overloaded constructor of SampleImage class
+     *
+     * Creates a SampleImage object; initialises image with a path and will set isValid to true if the path is valid.
+     *
+     * @param[in]     const char * newPath    The path of the image
+     */
+    SampleImage(const char *newPath);
+
+    /**
+     * @brief Sets path of image
+     *
+     * Sets the path of the SampleImage image by asking the user for input.
+     */
+    void setPath();
+    /**
+     * @brief Sets path of image
+     *
+     * Sets the path of the SampleImage image with a new const char * path. If the newPath is invalid, isValid will become false.
+     *
+     * @param[in]     const char * newPath    The path of the image
+     */
+    void setPath(const char *newPath);
+    /**
+     * @brief Sets path of image
+     *
+     * Sets the path of the SampleImage image using the path of an other SampleImage image.
+     *
+     * @param[in]     SampleImage & other    The path of an other SampleImage image
+     */
+    void setPath(SampleImage &other);
+
+    /**
+     * @brief Gets path of image
+     *
+     * Returns the private variable 'path'; a simple getter function.
+     *
+     * @return char * path
+     */
+    char *getPath();
+    /**
+     * @brief Gets image
+     *
+     * Returns the private variable 'image'; a simple getter function.
+     *
+     * @return cv::Mat image
+     */
+    cv::Mat getImage();
+    /**
+     * @brief Returns whether path is valid
+     *
+     * Returns the member variable 'isValid'. Is true if the path of an image is valid; false if it is not.
+     *
+     * @return bool isValid
+     */
+    bool isValid();
 };
 
 #endif // SAMPLE_IMAGE_HPP
