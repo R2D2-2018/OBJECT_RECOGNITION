5a2b86c3a560ccb391a9bbd1fb220f1f6a2ccff6
RoxanneMango <roxannevdpol@gmail.com>
2018-06-22 20:17:41 +0200

    > Added an empty ImageObject class in /src/image_object.cpp /src/image_object.hpp

---
+++ src/image_object.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file	  image_object.cpp
+ * @brief     Contains the ImageObject class
+ * @author    Roxanne van der Pol
+ * @license   MIT
+ */
+
+#include "image_object.hpp"
+
+ImageObject::ImageObject() {
+}
---
+++ src/image_object.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file	  image_object.hpp
+ * @brief     Contains the ImageObject class
+ * @author    Roxanne van der Pol
+ * @license   MIT
+ */
+
+#ifndef IMAGE_OBJECT_HPP
+#define IMAGE_OBJECT_HPP
+
+class ImageObject {
+  private:
+  public:
+    ImageObject();
+};
+
+#endif // IMAGE_OBJECT_HPP
