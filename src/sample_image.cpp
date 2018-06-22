7a7375a8604af4401a4d58183050120254729236
RoxanneMango <roxannevdpol@gmail.com>
2018-06-22 16:54:06 +0200

    > Added empty sample_image class in sample_image header and source files.

---
+++ src/sample_image.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file	  sample_image.cpp
+ * @brief     SampleImage class that contains the sample library to extract features from.
+ * @author    Roxanne van der Pol
+ * @license   MIT
+ */
+
+#include "sample_image.hpp"
+
+SampleImage::SampleImage() {	
+}
---
+++ src/sample_image.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file	  sample_image.hpp
+ * @brief     SampleImage class that contains the sample library to extract features from.
+ * @author    Roxanne van der Pol
+ * @license   MIT
+ */
+
+#ifndef SAMPLE_IMAGE_HPP
+#define SAMPLE_IMAGE_HPP
+
+class SampleImage {
+private:
+	
+public:
+	SampleImage();
+	
+};
+
+#endif // SAMPLE_IMAGE_HPP
